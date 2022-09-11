1fd7657832a39f11b32f035d16a4ff7955aa7fd5
garfbit <104676660+garfbit@users.noreply.github.com>
2022-09-11 09:31:09 -0500

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+let numero = 0
+let Potencia = 0
+let Resultado = 0
 basic.forever(function () {
-	
+    numero = 5
+    Potencia = 10
+    Resultado = numero ** Potencia
+    basic.showNumber(Resultado)
 })
